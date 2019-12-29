6ddd1403550de70447a8094468bf1af511610c00
Alexandr Timofeev <minimonium@outlook.com>
2019-12-30 02:11:54 +0300

    Figure out what's inside the github.context

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hello, World!')
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`)
+
+  console.log(github.context)
+} catch (error) {
+  core.setFailed(error.message);
+}
