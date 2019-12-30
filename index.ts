8a674cf4c74f217dfde5f70af2b0338aa5854435
Alexandr Timofeev <minimonium@outlook.com>
2019-12-30 03:35:06 +0300

    Print the hash of the tarball

--- index.ts
+++ index.ts
@@ -1,9 +1,22 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
+import {createHash} from 'crypto';
+import {get} from 'request-promise';
 
 try {
   const tarball_url = github.context.payload.release.tarball_url;
-  console.log(`The tarball: ${tarball_url}`);
+  console.log('The tarball: ${tarball_url}');
+
+  let shasum = createHash('sha256');
+  get({uri: tarball_url})
+      .then(body => {
+        shasum.update(body);
+        const hash = shasum.digest('hex');
+        console.log(tarball_url + ' ' + hash);
+      })
+      .catch(e => {
+        console.log('error!');
+      });
 } catch (error) {
   core.setFailed(error.message);
 }
